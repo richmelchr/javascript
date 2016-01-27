ba586e79439d542f7c793f0cc5e80200f3079761
Richard Melcher <richmelchr@gmail.com>
2016-01-27 00:34:19 -0700

    init commit

---
+++ java_notes.js
@@ -0,0 +1,13 @@
+/**
+ * Created by richmelchr on 1/26/2016.
+ */
+var a = 5;
+var b = "5"; // quotation mark's make this a string
+var c = 5;
+
+if (a == c) {
+    console.log("they equal");
+} else {
+    alert("not equal");
+}
+
