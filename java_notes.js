975d9b7f370a9eb84fa4025be6249d377663f6bc
Richard Melcher <richmelchr@gmail.com>
2016-02-03 14:30:42 -0700

    stopping point, useful javascript methods

--- java_notes.js
+++ java_notes.js
@@ -14,18 +14,32 @@ if (a == c) {
 //&& logical and
 //|| logical or
 
-//----------------------------------------------------------------
+//--------Ternary Operator--------------------------------------
 var playerOne = 500;
 var playerTwo = 600;
 
 var highScore = (playerOne > playerTwo) ? playerOne : playerTwo;
-
+//the above and below statements are exactly the same.
 if (playerOne > playerTwo) {
     highScore = playerOne;
 } else {
     highScore = playerTwo;
 }
+//-Console------------------------------------------------------
+console.log("testing console");
 
-if (a > b) {
-    //this stuff
+//--useful methods------------------------------------------------------
+var phrase = "this is some sample text";
+
+console.log(phrase.length); //output will be 24
+console.log(phrase.toUpperCase()); //output 'phrase' in all uppercase
+var words = phrase.split(" "); //split this string into an array on a space
+var position = phrase.indexOf("sample"); //var will equal 13
+
+if (phrase.indexOf("absent") == -1) { //indexOf returns -1 if
+    console.log("this word does not occur.");
 }
+//-------------------------------------------------------------
+
+
+//03 Types and Objects // Strings
